fcaf841a6650d29c040d96ed3dafc7ff797b1fff
CHOICEHOTELS\kevin.baker <kevin.baker@choicehotels.com>
2023-07-11 11:52:13 -0700

    Server Side Search functional

--- src/components/pages/FilterPage.js
+++ src/components/pages/FilterPage.js
@@ -1,93 +1,115 @@
-import React, { Component } from 'react';
+import * as React from 'react';
+import axios from 'axios';
 import {
-    Table,
-    Header,
-    HeaderRow,
-    HeaderCell,
-    Body,
-    Row,
-    Cell,
+  Table,
+  Header,
+  HeaderRow,
+  HeaderCell,
+  Body,
+  Row,
+  Cell,
+  useCustom,
 } from '@table-library/react-table-library/table';
+//Source https://www.robinwieruch.de/react-server-side-table/
 
-const list = [
-    {
-        id: '1',
-        name: 'VSCode',
-        deadline: new Date(2020, 1, 17),
-        type: 'SETUP',
-        isComplete: true,
-    },
-    {
-        id: '2',
-        name: 'JavaScript',
-        deadline: new Date(2020, 2, 28),
-        type: 'LEARN',
-        isComplete: true,
-    },
-    {
-        id: '3',
-        name: 'React',
-        deadline: new Date(2020, 3, 8),
-        type: 'LEARN',
-        isComplete: false,
-    }
-];
+const BASE_URL = 'http://hn.algolia.com/api/v1/search';
+
+const INITIAL_PARAMS = {
+  search: '',
+};
 
 const FilterPage = () => {
-      const [search, setSearch] = React.useState('');
+    const [data, setData] = React.useState({ nodes: [] });
+
+  const fetchData = React.useCallback(async (params) => {
+    const url = `${BASE_URL}?query=${params.search}`;
+    const result = await axios.get(url);
+
+
+    setData({ nodes: result.data.hits });
+  }, []);
+
+  React.useEffect(() => {
+    fetchData({
+      search: INITIAL_PARAMS.search,
+    });
+  }, [fetchData]);
+    
+  // server-side search
+  const [search, setSearch] = React.useState(INITIAL_PARAMS.search);
 
   const handleSearch = (event) => {
     setSearch(event.target.value);
   };
 
- const data = {
-    nodes: list.filter((item) =>
-    item.name.toLowerCase().includes(search.toLowerCase())
-    ),
-  };
+    useCustom('search', data, {
+    state: { search },
+    onChange: onSearchChange,
+  });
 
-    return (
-     <React.Fragment>
-          <label htmlFor="search">
-        Search by Task:
-        <input id="search" type="text" onChange={handleSearch} />
+    // listeners
+      const timeout = React.useRef();
+
+  function onSearchChange(action, state) {
+   if (timeout.current) clearTimeout(timeout.current);
+
+    timeout.current = setTimeout(
+      () =>
+        fetchData({
+          search: state.search,
+        }),
+      500
+    );
+  }
+  return (
+      <React.Fragment>
+       <label htmlFor="search">
+        Search by Title:
+        <input
+          id="search"
+          type="text"
+          value={search}
+          onChange={handleSearch}
+        />
       </label>
-      
-            <Table data={data}>
-            {(tableList) => (
-                <React.Fragment>
-                    <Header>
-                        <HeaderRow>
-                            <HeaderCell>Task</HeaderCell>
-                            <HeaderCell>Deadline</HeaderCell>
-                            <HeaderCell>Type</HeaderCell>
-                            <HeaderCell>Complete</HeaderCell>
-                        </HeaderRow>
-                    </Header>
-                    <Body>
-                        {tableList.map((item) => (
-                            <Row key={item.id} item={item}>
-                                <Cell>{item.name}</Cell>
-                                <Cell>
-                                    {item.deadline.toLocaleDateString(
-                                        'en-US',
-                                        {
-                                            year: 'numeric',
-                                            month: '2-digit',
-                                            day: '2-digit',
-                                        }
-                                    )}
-                                </Cell>
-                                <Cell>{item.type}</Cell>
-                                <Cell>{item.isComplete.toString()}</Cell>
-                            </Row>
-                        ))}
-                    </Body>
-                </React.Fragment>
-                )}
-            </Table>
-            </React.Fragment>
-        );
+    <Table data={data}>
+      {(tableList) => (
+         <React.Fragment>
+
+          <Header>
+            <HeaderRow>
+              <HeaderCell>Title</HeaderCell>
+              <HeaderCell>Created At</HeaderCell>
+              <HeaderCell>Points</HeaderCell>
+              <HeaderCell>Comments</HeaderCell>
+            </HeaderRow>
+          </Header>
+
+          <Body>
+            {tableList.map((item) => (
+              <Row key={item.objectID} item={item}>
+                <Cell>
+                  <a href={item.url}>{item.title}</a>
+                </Cell>
+                <Cell>
+                  {new Date(
+                    item.created_at
+                  ).toLocaleDateString('en-US', {
+                    year: 'numeric',
+                    month: '2-digit',
+                    day: '2-digit',
+                  })}
+                </Cell>
+                <Cell>{item.points}</Cell>
+                <Cell>{item.num_comments}</Cell>
+              </Row>
+            ))}
+          </Body>
+         </React.Fragment>
+      )}
+    </Table>
+    </React.Fragment>
+  );
 
 }
 
